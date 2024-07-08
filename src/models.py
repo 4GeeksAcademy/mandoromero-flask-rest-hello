5e419184d984309c8af374199c1763076471310b
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2024-07-08 14:52:53 +0000

    issues

--- src/models.py
+++ src/models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -19,6 +20,7 @@ class User(db.Model):
         }
 
 class People(db.Model):
+    __tablename__ = "people"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
     age = db.Column(db.Integer, nullable=False)
@@ -34,6 +36,7 @@ class People(db.Model):
         }
 
 class Planets(db.Model):
+    __tablename__ = "planets"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
     population = db.Column(db.Integer, nullable=False)
